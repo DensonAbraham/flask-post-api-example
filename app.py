123112ade575ed4bd6ac0203bcc93fd969e5abf6
Denson Abraham <densonabraham98@gmail.com>
2022-08-12 21:11:58 +0530

    added cors

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask import request
+from flask_cors import CORS
 app = Flask(__name__) 
 
-
+CORS(app)
 @app.route('/hello/', methods=['GET', 'POST'])
 def hello():
     return "got POST request"
