b7f5f35e8a5d7f09da30bfdfe445fb61029afa27
Denson Abraham <densonabraham98@gmail.com>
2022-08-13 11:26:41 +0530

    returning request values

--- app.py
+++ app.py
@@ -5,5 +5,7 @@ app = Flask(__name__)
 
 CORS(app)
 @app.route('/hello/', methods=['GET', 'POST'])
-def hello():
-    return "got POST request"
+def hello(): 
+    data = request.get_json()
+    data = data.get("title")
+    return data
